0947de2e2d9103e9cc2fc88818e87b953debe0e1
Arturo Reygadas <a.reygadas.g@gmail.com>
2020-02-16 12:59:51 -0600

    20.1.20

--- Backend/routes/index.js
+++ Backend/routes/index.js
@@ -2,19 +2,21 @@ const express = require('express');
 const router = express.Router();
 const pool = require('../database');
 
-router.get('/DBA', function(req, res, next) {
-     pool.query('show databases', function (error, results, fields) {
+router.get('/DBA' , async (req, res) => {
+        await pool.query('Show databases', function (error, results, fields) {
         if(error) throw error;
         res.send(JSON.stringify(results));
     });
 });
 
-router.get('/tablas', function(req, res, next) {
-    console.log()   
-    //pool.query("SHOW TABLES FROM latinoamricana", function (error, results, fields) {
-        pool.query("SHOW TABLES FROM "+req.body.name, function (error, results, fields) {
-        if(error) throw error;
-        res.send(JSON.stringify(results));
-    });
+
+router.get('/Tabs', async (req, res) => {
+    await pool.query('Show databases', function (error, results, fields) {
+    if(error) throw error;
+    res.send(JSON.stringify(results));
 });
+});
+
+
+
 module.exports = router;
--- src/Pages/DBAdministador.js
+++ src/Pages/DBAdministador.js
@@ -8,9 +8,9 @@ export default class Users extends Component {
         this.variable ='v'
         this.state = {
             bases: [],
-            tablas:[],
-            columnas1:[],
-            columnas2:[],
+            tablas: [],
+            columnas1: [],
+            columnas2: [],
             
             
         }
@@ -22,7 +22,7 @@ export default class Users extends Component {
             method: 'GET'
         }).then(function(response) {
             if (response.status >= 400) {
-                throw new Error("Bad response from server");
+                throw new Error("Error 1");
             }
             return response.json();
         }).then(function(data) {
@@ -30,62 +30,33 @@ export default class Users extends Component {
         }).catch(err => {
         console.log('caught it!',err);
         })
-    }
-
-
-    OptionChange = e => {
-        console.log("El campo ha cambiado")
-       this.variable = 'Tables_in_'.concat(e.target.value)
-        console.log(this.variable);
-        
-        // let self = this;
-        // fetch("/tablas", {
-        //     method: 'POST',
-        //     body: JSON.stringify(this.variable)
-        // }).then(function(response) {
-        //     if (response.status >= 400) {
-        //         throw new Error("Bad response from server");
-        //     }
-        //     return response.json();
-        // }).then(function(data) {
-        //     self.setState({tablas: data});
-        // }).catch(err => {
-        // console.log('caught it!',err);
-        // })
         
     }
-    handleTab(event) {
-        //Edit functionality
-        event.preventDefault()
-        var data = {
-           // name: event.target.value
-            name: 'db_links'
-        }
-        fetch("/tablas", {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(data)
+
+    handleChange() {
+        let self = this;
+        fetch('/Tabs', {
+            method: 'GET'
         }).then(function(response) {
             if (response.status >= 400) {
-                throw new Error("Bad response from server");
+                throw new Error("No hay respuesta en la DBA");
             }
             return response.json();
         }).then(function(data) {
-            this.setState({tablas: data});
-        }).catch(function(err) {
-            console.log(err)
-        });
+            self.setState({tablas: data});
+        }).catch(err => {
+        console.log('caught it!',err);
+        })
     }
 
+    
     render() {
         return (
         <MDBContainer> 
             <MDBRow>
                 <MDBCol size='4'>
                     <h2>Base de Datos</h2>
-                    <select className="browser-default custom-select" onChange={this.handleTab}>
+                    <select className="browser-default custom-select" onChange={this.handleChange}>
                         {this.state.bases.map(base =>
                             <option value={base.id}>{base.Database} </option>
                         )}
@@ -95,11 +66,10 @@ export default class Users extends Component {
             <MDBRow>
                 <MDBCol size='4'>
                     <h2>Tablas</h2>
-                    <select className="browser-default custom-select" onChange={this.OptionChange}>
+                    <select className="browser-default custom-select" >
                         {this.state.tablas.map(tabla =>
-                            <option value={tabla.id}>{tabla.this.variable} </option>
+                            <option value={tabla.id}>{tabla.Database} </option>
                         )}
-                        console.log(this.variable)
                     </select>
                 </MDBCol>
             </MDBRow>
